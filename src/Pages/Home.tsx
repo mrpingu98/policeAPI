c852c930df3c97d98abe736bf67078b29069c16b
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-02-24 15:32:42 +0000

    Adding navBar component to homepage

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { NavBar } from "../Components/NavBar";
 
 const Home: React.FC = () => {
   return (
-    <div className="centre">
-      <h1>Portfolio / POLICE API?</h1>
-      <p>This is a personal project made by.... brief explanation of site?</p>
-    </div>
+    <>
+      <NavBar />
+      <div className="centre">
+        <h1>Portfolio / POLICE API?</h1>
+        <p>This is a personal project made by.... brief explanation of site?</p>
+      </div>
+    </>
   );
 };
 
