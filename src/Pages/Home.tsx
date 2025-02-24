106558673a1a75f14300dafd01d21ab3db42769e
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-02-24 13:59:10 +0000

    removing test button from homep age

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-import "../Styling/main.scss";
-import { Button } from "../Components/Button";
 
 const Home: React.FC = () => {
   return (
     <div className="centre">
       <h1>Portfolio / POLICE API?</h1>
       <p>This is a personal project made by.... brief explanation of site?</p>
-      <Button text={"hello"} onClick={() => console.log("button clicked")} />
     </div>
   );
 };
