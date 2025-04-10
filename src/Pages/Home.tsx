eb6ccf5ab660db1fe2102226b8715dccc35981eb
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-04-10 15:20:25 +0100

    conteinr for background image

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,15 +1,19 @@
 import React from "react";
+import "../Styling/pages/home.scss";
 
 const Home: React.FC = () => {
   return (
-    <div className="flexCenter">
-      <p>
-        This website uses the UK Police API to gather data about crime and
-        policing across England, Wales and Northern Ireland
-      </p>
-      <p>Head to the API page to find out about crime in your local area</p>
-      <img src="/assets/ukPoliceLogo.jpg" alt="police logo" />
-    </div>
+    <>
+      <div className="titleContainer">
+        <div>
+          <p>
+            This website uses the UK Police API to gather data about crime and
+            policing across England, Wales and Northern Ireland
+          </p>
+          <p>Head to the API page to find out about crime in your local area</p>
+        </div>
+      </div>
+    </>
   );
 };
 
