e355b08370790c8d17797f4e01285ff1987d4760
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-04-10 16:08:41 +0100

    removed unnecessary React fragment

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,19 +3,17 @@ import "../Styling/pages/home.scss";
 
 const Home: React.FC = () => {
   return (
-    <>
-      <div className="welcomeMessageContainer">
-        <div>
-          <p className="welcomeMessage">
-            This website uses the UK Police API to gather data about crime and
-            policing across England, Wales and Northern Ireland
-          </p>
-          <p className="welcomeMessage">
-            Head to the API page to find out about crime in your local area
-          </p>
-        </div>
+    <div className="welcomeMessageContainer">
+      <div>
+        <p className="welcomeMessage">
+          This website uses the UK Police API to gather data about crime and
+          policing across England, Wales and Northern Ireland
+        </p>
+        <p className="welcomeMessage">
+          Head to the API page to find out about crime in your local area
+        </p>
       </div>
-    </>
+    </div>
   );
 };
 
