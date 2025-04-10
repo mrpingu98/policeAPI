5eec95a73b913d66267fd729c9904943abd59e24
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-04-10 16:04:24 +0100

    styling for text and background image container

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -4,13 +4,15 @@ import "../Styling/pages/home.scss";
 const Home: React.FC = () => {
   return (
     <>
-      <div className="titleContainer">
+      <div className="welcomeMessageContainer">
         <div>
-          <p>
+          <p className="welcomeMessage">
             This website uses the UK Police API to gather data about crime and
             policing across England, Wales and Northern Ireland
           </p>
-          <p>Head to the API page to find out about crime in your local area</p>
+          <p className="welcomeMessage">
+            Head to the API page to find out about crime in your local area
+          </p>
         </div>
       </div>
     </>
