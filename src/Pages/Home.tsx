5fddc385de92985976516cc1667de1a2bd3d3307
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-04-11 14:19:14 +0100

    recent news title

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,17 +3,21 @@ import "../Styling/pages/home.scss";
 
 const Home: React.FC = () => {
   return (
-    <div className="welcomeMessageContainer">
-      <div>
-        <p className="welcomeMessage">
-          This website uses the UK Police API to gather data about crime and
-          policing across England, Wales and Northern Ireland
-        </p>
-        <p className="welcomeMessage">
-          Head to the API page to find out about crime in your local area
-        </p>
+    <>
+      <div className="welcomeMessageContainer">
+        <div>
+          <p className="welcomeMessage">
+            This website uses the UK Police API to gather data about crime and
+            policing across England, Wales and Northern Ireland
+          </p>
+          <p className="welcomeMessage">
+            Head to the API page to find out about crime in your local area
+          </p>
+        </div>
       </div>
-    </div>
+      <h2 className="recentNewsTitle">Recent News</h2>
+      <div className="recentNewsContainer">test</div>
+    </>
   );
 };
 
