8e8f4d75ddd62d2d71cb6610ba9142e0a93c6e19
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-04-16 15:10:56 +0100

    basic footer styling

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import "../Styling/components/footer.scss";
 
 const Footer: React.FC = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <div>FOOTER1</div>
+      <img src="/assets/police-logo.jpg" />
+    </div>
+  );
 };
 
 export { Footer };
