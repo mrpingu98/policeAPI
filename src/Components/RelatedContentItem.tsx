cf2fe6ed203ae63ce9fc77d3f36f803a8e7ac0f7
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-04-16 17:11:24 +0100

    RelatedContentItem component made

---
+++ src/Components/RelatedContentItem.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RelatedContentItem: React.FC = () => {
+  return (
+    <div>
+      <div>related content</div>
+    </div>
+  );
+};
+
+export { RelatedContentItem };
