c5d9b96ec05e3b05de292b05ee295839a72393ec
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-04-16 16:59:51 +0100

    increased padding

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -4,11 +4,10 @@ import "../Styling/components/footer.scss";
 const Footer: React.FC = () => {
   return (
     <div className="footerContainer">
-      <div className="footerContentContainer">
-        <div className="footerContent">Tel: 033 3333 3333</div>
-        <div className="footerContent">Email: helpme@police.co.uk</div>
-        <div className="footerContent">FAQs</div>
-        <div>Remember - Call 999 in an emergency</div>
+      <div className="footerInformationContainer">
+        <div className="footerInformationItem">Tel: 033 3333 3333</div>
+        <div className="footerInformationItem">Email: helpme@police.co.uk</div>
+        <div className="footerInformationItem">FAQs</div>
       </div>
       <img className="logo" src="/assets/police-logo.jpg" />
     </div>
