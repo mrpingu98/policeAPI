7c96fac845a55da2471a795d6120cbd4072e4595
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-04-22 11:17:07 +0100

    updated styling

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -5,9 +5,9 @@ const Footer: React.FC = () => {
   return (
     <div className="footerContainer">
       <div className="footerInformationContainer">
-        <div className="footerInformationItem">Tel: 033 3333 3333</div>
-        <div className="footerInformationItem">Email: helpme@police.co.uk</div>
-        <div className="footerInformationItem">FAQs</div>
+        <p className="footerInformationItem">Tel: 033 3333 3333</p>
+        <p className="footerInformationItem">Email: helpme@police.co.uk</p>
+        <p className="footerInformationItem">FAQs</p>
       </div>
       <img className="logo" src="/assets/police-logo.jpg" />
     </div>
