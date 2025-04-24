d936c586758a71168020fd75e23df65ada7649e1
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-04-24 13:48:31 +0100

    blogs posts examples

---
+++ src/Components/NewsItem.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "../Styling/components/newsItem.scss";
+import "../Styling/layout/layout.scss";
+
+interface NewsItemProps {
+  title: string;
+}
+
+// const NewsItem: React.FC<NewsItemProps> = ({ title }) => {
+//   return (
+//     <div className="newsItemContainer">
+//       <div className="titleContainer">
+//         <p className="noMarginBottom">{title}</p>
+//       </div>
+//     </div>
+//   );
+// };
+
+const NewsItem: React.FC<NewsItemProps> = (props) => {
+  return (
+    <div className="newsItemContainer">
+      <div className="titleContainer">
+        <p className="noMarginBottom">{props.title}</p>
+      </div>
+    </div>
+  );
+};
+
+export { NewsItem };
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,59 +1,37 @@
 import React from "react";
-import "../Styling/pages/home.scss";
-import { RecentNewsItem } from "../Components/RecentNewsItem";
-import { RelatedContentItem } from "../Components/RelatedContentItem";
+// import { NewsItem } from "../Components/NewsItem";
+import { Button } from "../Components/Button";
 
 const Home: React.FC = () => {
+  function buttonClickedMessageOne() {
+    return console.log(
+      "This is the first message that can be called and logged from the Home page"
+    );
+  }
+
+  function buttonClickedMessageTwo() {
+    return console.log(
+      "This is the second message that can be called and logged from the Home page"
+    );
+  }
+
   return (
-    <>
-      <div className="welcomeMessageContainer">
-        <div>
-          <p className="welcomeMessage">
-            This website uses the UK Police API to gather data about crime and
-            policing across England, Wales and Northern Ireland
-          </p>
-          <p className="welcomeMessage">
-            Head to the API page to find out about crime in your local area
-          </p>
-        </div>
-      </div>
-      <div className="recentNewsContainer">
-        <h2 className="title">Recent News</h2>
-        <div className="recentNewsItemContainer">
-          <RecentNewsItem
-            title={"Man spontanesouly combusts - is this the end?"}
-            imageSource={"/assets/spontaneous-combustion.jpg"}
-            alternativeImageText={"burnt outline of man in seat"}
-          />
-          <RecentNewsItem
-            title={"Tortoise conducts traffic stop in world's first"}
-            imageSource={"/assets/tortoise-officer.jpg"}
-            alternativeImageText={"tortoise police officer"}
-          />
-          <RecentNewsItem
-            title={
-              "Five-year old charged with espionage after passing on teaching material to rival school"
-            }
-            imageSource={"/assets/espionage.jpg"}
-            alternativeImageText={"gazing eye"}
-          />
-        </div>
-      </div>
-      <h2 className="title">Related Content</h2>
-      <div className="relatedContentContainer">
-        <RelatedContentItem
-          imageSource="/assets/internal-police-reports.jpg"
-          title="Internal police reports"
-          href=""
-        />
-        <RelatedContentItem
-          imageSource="/assets/local-community.jpg"
-          title="Local community initiatives"
-          href=""
-        />
-      </div>
-    </>
+    <div>
+      <p>This the home page</p>
+      <Button
+        text="Click me for message one"
+        onClick={buttonClickedMessageOne}
+      />
+      <Button
+        text="Click me for message two"
+        onClick={buttonClickedMessageTwo}
+      />
+    </div>
   );
 };
 
 export { Home };
+
+// const buttonClickeds = () => {
+//   return console.log("This has been called and logged from the Home page");
+// };
