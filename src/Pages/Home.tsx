d8f03fb0eeef2b2f207cdc693816c30f27667484
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-05-19 10:41:47 +0100

    reorganising Home page code

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -17,30 +17,28 @@ const Home: React.FC = () => {
           </p>
         </div>
       </div>
+      <h2 className="title">Recent News</h2>
       <div className="recentNewsContainer">
-        <h2 className="title">Recent News</h2>
-        <div className="recentNewsItemContainer">
-          <div className="mainNewsItem">
-            <RecentNewsItem
-              title={"Man spontanesouly combusts - is this the end?"}
-              imageSource={"/assets/spontaneous-combustion.jpg"}
-              alternativeImageText={"burnt outline of man in seat"}
-            />
-          </div>
-          <div className="secondaryNewsItems">
-            <RecentNewsItem
-              title={"Tortoise conducts traffic stop in world's first"}
-              imageSource={"/assets/tortoise-officer.jpg"}
-              alternativeImageText={"tortoise police officer"}
-            />
-            <RecentNewsItem
-              title={
-                "Five-year old charged with espionage after passing on teaching material to rival school"
-              }
-              imageSource={"/assets/espionage.jpg"}
-              alternativeImageText={"gazing eye"}
-            />
-          </div>
+        <div className="mainNewsItem">
+          <RecentNewsItem
+            title={"Man spontanesouly combusts - is this the end?"}
+            imageSource={"/assets/spontaneous-combustion.jpg"}
+            alternativeImageText={"burnt outline of man in seat"}
+          />
+        </div>
+        <div className="secondaryNewsItems">
+          <RecentNewsItem
+            title={"Tortoise conducts traffic stop in world's first"}
+            imageSource={"/assets/tortoise-officer.jpg"}
+            alternativeImageText={"tortoise police officer"}
+          />
+          <RecentNewsItem
+            title={
+              "Five-year old charged with espionage after passing on teaching material to rival school"
+            }
+            imageSource={"/assets/espionage.jpg"}
+            alternativeImageText={"gazing eye"}
+          />
         </div>
       </div>
       <h2 className="title">Related Content</h2>
