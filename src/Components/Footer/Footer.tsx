4f8dcf7149249696610587708bba176a2689fa21
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-06-02 14:26:36 +0100

    adding data test-ids to Footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -5,12 +5,19 @@ const Footer: React.FC = () => {
   return (
     <div className="footerContainer">
       <div className="footerInformationContainer">
-        <p className="footerInformationItem">Tel: 033 3333 3333</p>
-        <p className="footerInformationItem">Email: helpme@police.co.uk</p>
-        <p className="footerInformationItem">FAQs</p>
+        <p className="footerInformationItem" data-testid="footer-telephone">
+          Tel: 033 3333 3333
+        </p>
+        <p className="footerInformationItem" data-testid="footer-email">
+          Email: helpme@police.co.uk
+        </p>
+        <p className="footerInformationItem" data-testid="footer-faqs">
+          FAQs
+        </p>
       </div>
       <img
         className="logo"
+        data-testid="footer-logo"
         src={process.env.PUBLIC_URL + "/assets/police-logo.jpg"}
         alt="Uk police logo"
       />
