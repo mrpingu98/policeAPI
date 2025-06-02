a8522b33953b38cc8c5a5edb38a2ec754f0fb93c
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-06-02 15:00:13 +0100

    Initial realted content itme tests

---
+++ src/Components/RelatedContentItem/RelatedContentItem.spec.tsx
@@ -0,0 +1,38 @@
+import { render } from "@testing-library/react";
+import { RelatedContentItem } from "./RelatedContentItem";
+import React from "react";
+
+const MockRelatedContentItem = () => {
+  return (
+    <RelatedContentItem
+      imageSource={"/assets/spontaneous-combustion.jpg"}
+      title={"Test Title"}
+      href={""}
+    />
+  );
+};
+
+describe("Related Content Item tests", () => {
+  it("should render a title", async () => {
+    const { getByTestId } = render(<MockRelatedContentItem />);
+    const title = getByTestId("relatedContent-title");
+    expect(title.textContent).toBe("Test Title");
+  });
+
+  it("should render an image", async () => {
+    const { getByTestId } = render(<MockRelatedContentItem />);
+    const image = getByTestId("relatedContent-image");
+    expect(image).toBeInTheDocument();
+  });
+
+  it("should render a link", async () => {
+    const { getByTestId } = render(<MockRelatedContentItem />);
+    const link = getByTestId("relatedContent-link");
+    expect(link).toBeInTheDocument();
+  });
+});
+
+//renders a title
+//render an image
+//render a link
+//click the href and page reloads?
