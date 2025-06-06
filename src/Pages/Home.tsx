5fa4c02842f333d45a3f88330f8a430353db5a03
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-06-06 12:58:33 +0100

    removing unecesssary div

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -7,16 +7,14 @@ const Home: React.FC = () => {
   return (
     <>
       <div className="welcomeMessageContainer">
-        <div>
-          <p className="welcomeMessage">
-            This website uses the UK Police API to gather data about crime and
-            policing across England, Wales and Northern Ireland
-          </p>
-          <br />
-          <p className="welcomeMessage">
-            Head to the API page to find out about crime in your local area
-          </p>
-        </div>
+        <p className="welcomeMessage">
+          This website uses the UK Police API to gather data about crime and
+          policing across England, Wales and Northern Ireland
+        </p>
+        <br />
+        <p className="welcomeMessage">
+          Head to the API page to find out about crime in your local area
+        </p>
       </div>
       <h2 className="title">Recent News</h2>
       <div className="recentNewsContainer">
