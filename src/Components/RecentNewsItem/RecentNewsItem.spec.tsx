72d593a3df248e4a3c07c003984a780140b8c412
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-06-06 10:15:42 +0100

    UPATING TO BEST-PRACTICE FORMATTING

--- src/Components/RecentNewsItem/RecentNewsItem.spec.tsx
+++ src/Components/RecentNewsItem/RecentNewsItem.spec.tsx
@@ -15,73 +15,109 @@ const renderComponent = (props = {}) =>
 //we spread the default props in
 //then spread additional/overriding props in as well
 
-describe("Normal Recent News Item tests", () => {
-  it("should render the correct title", async () => {
+describe("Recent News Item tests", () => {
+  //#region props
+  it("SHOULD render the title WHEN a non-empty string is passed", async () => {
+    //arrange
     const { getByTestId } = renderComponent();
+
+    //act
     const title = getByTestId("recent-news-title");
+
+    //assert
     expect(title.textContent).toBe("Test Title");
   });
 
-  it("should render the correct title", async () => {
+  it("SHOULD render correct image WHEN a non-empty string is passed", async () => {
+    //arrange
     const { getByTestId } = renderComponent();
-    const title = getByTestId("recent-news-title");
-    expect(title.textContent).toBe("Test Title");
-  });
 
-  it("should render correct image", async () => {
-    const { getByTestId } = renderComponent();
+    //act
     const image = getByTestId("recent-news-image");
+
+    //assert
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute("alt", "Test Alternative Text");
     expect(image).toHaveAttribute("src", "/assets/spontaneous-combustion.jpg");
   });
+  //#endregion
 
-  it("should render an image with the correct alternative text", async () => {
+  //#region mainNewsItem = false
+  it("SHOULD render correct className for the main container WHEN mainNewsItem is false", async () => {
+    //arrange
     const { getByTestId } = renderComponent();
-    const image = getByTestId("recent-news-image");
-    expect(image).toHaveAttribute("alt", "Test Alternative Text");
-  });
 
-  it("should render correct className for the main container", async () => {
-    const { getByTestId } = renderComponent();
+    //act
     const mainContainer = getByTestId("recent-news-main-container");
+
+    //assert
     expect(mainContainer).toHaveClass("newsItemContainer");
   });
 
-  it("should render correct className for the image container", async () => {
+  it("SHOULD render correct className for the image container when mainNewsItem is false", async () => {
+    //arrange
     const { getByTestId } = renderComponent();
+
+    //act
     const imageContainer = getByTestId("recent-news-image");
+
+    //assert
     expect(imageContainer).toHaveClass("imageContainer");
   });
 
-  it("should not render mainTitleContainer className for title container", async () => {
+  it("SHOULD not render mainTitleContainer className for the title container WHEN mainNewsItem is false", async () => {
+    //arrange
     const { getByTestId } = renderComponent();
+
+    //act
     const titleContainer = getByTestId("recent-news-title-container");
+
+    //assert
     expect(titleContainer).not.toHaveClass("titleContainer mainTitleContainer");
   });
-});
 
-describe("Main Recent News Item tests", () => {
-  it("should render mainNewsItem className for main container", async () => {
+  //#endregion
+
+  //#region mainNewsItem = true
+  it("SHOULD render mainNewsItem className for main container WHEN mainNewsItem is true", async () => {
+    //arrange
     const { getByTestId } = renderComponent({ mainNewsItem: true });
+
+    //act
     const mainContainer = getByTestId("recent-news-main-container");
+
+    //assert
     expect(mainContainer).toHaveClass(
       "newsItemContainer mainNewsItemContainer"
     );
   });
 
-  it("should render mainImageContainer className for the image", async () => {
+  it("SHOULD render mainImageContainer className for the image WHEN mainNewsItem is true", async () => {
+    //arrange
     const { getByTestId } = renderComponent({ mainNewsItem: true });
+
+    //act
     const mainContainer = getByTestId("recent-news-image");
+
+    //assert
     expect(mainContainer).toHaveClass("imageContainer mainImageContainer");
   });
 
-  it("should render mainTitleContainer className for the title container", async () => {
+  it("SHOULD render mainTitleContainer className for the title container WHEN mainNewsItem is true", async () => {
+    //arrange
     const { getByTestId } = renderComponent({ mainNewsItem: true });
-    const mainContainer = getByTestId("recent-news-title-container");
-    expect(mainContainer).toHaveClass("titleContainer mainTitleContainer");
-  });
-});
 
-//renders image (with alternative text)
-//title
-//mainNewsItemProp - check classnames of container, image and title change based on this
-//can also check non-existent classNames
+    //act
+    const mainContainer = getByTestId("recent-news-title-container");
+
+    //assert
+    expect(mainContainer).toHaveClass("titleContainer mainTitleContainer");
+
+    //#endregion
+  });
+
+  //need to think about tests from a 'wider' viewpoint, not just testing the component as is...
+  //if an empty string is passed for the title, what would we want to happen?
+  //if no alt text is given?
+  //etc.
+});
