0300c43b0935ecb093480925dd8259416b15f4f6
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-06-16 11:07:14 +0100

    updating index to use RouteProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { router } from "./router";
+import { RouterProvider } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
