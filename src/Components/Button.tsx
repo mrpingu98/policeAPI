e706d4d54d48b059bd727c0abddc630a9309df5a
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-06-25 15:27:32 +0100

    updating button component

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -2,12 +2,52 @@ import React from "react";
 import "../Styling/components/button.scss";
 
 interface ButtonProps {
-  text: string;
+  text?: string;
+  icon?: React.ReactNode;
+  datatestId?: string;
   onClick: () => void;
+  className: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
-  return <button onClick={onClick}>{text}</button>;
+const Button: React.FC<ButtonProps> = ({
+  text,
+  onClick,
+  className,
+  icon,
+  datatestId,
+}) => {
+  return (
+    <>
+      {text && (
+        <button
+          data-testId={datatestId}
+          className={className}
+          onClick={onClick}
+        >
+          {text}
+        </button>
+      )}
+      {icon && (
+        <button
+          data-testId={datatestId}
+          className={className}
+          onClick={onClick}
+        >
+          {icon}
+        </button>
+      )}
+      {icon && text && (
+        <button
+          data-testId={datatestId}
+          className={className}
+          onClick={onClick}
+        >
+          {text}
+          {icon}
+        </button>
+      )}
+    </>
+  );
 };
 
 export { Button };
