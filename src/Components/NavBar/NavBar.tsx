14cc8439fac470c4cb52ae6b630d168cc1101528
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-07-28 13:20:38 +0100

    adding safety checks to array for navbar

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../../Styling/components/navbar.scss";
 import { Link } from "react-router-dom";
 import { Routes } from "../Types";
@@ -7,6 +7,8 @@ import { useIsMobile } from "../../Hooks/useIsMobile";
 import { NonEmptyArray } from "../Types";
 
 const NavBar: React.FC = () => {
+  const [safeRoutesArray, setSafeRoutesArray] = React.useState<Routes[]>([]);
+
   const routes: NonEmptyArray<Routes> = [
     { routeUrl: "#", name: "Home" },
     { routeUrl: "#", name: "API" },
@@ -14,23 +16,40 @@ const NavBar: React.FC = () => {
 
   const isMobileView = useIsMobile();
 
+  const filterRoutesArray = () => {
+    const safeRoutes = routes.filter(
+      (route) => route.name.trim() && route.routeUrl.trim()
+    );
+    setSafeRoutesArray(safeRoutes);
+  };
+
+  useEffect(() => {
+    filterRoutesArray();
+  }, []);
+
   return (
     <div>
-      {isMobileView ? (
-        <div className="navDropdown" data-testid="nav-dropdown">
-          <NavbarDropdown routesArray={routes} />
-        </div>
-      ) : (
-        routes.map((route, index) => (
-          <Link
-            to={route.routeUrl}
-            key={index}
-            className="navLink"
-            data-testid="nav-links"
-          >
-            {route.name}
-          </Link>
-        ))
+      {safeRoutesArray &&
+        safeRoutesArray.length > 0 &&
+        (isMobileView ? (
+          <div className="navDropdown" data-testid="nav-dropdown">
+            <NavbarDropdown routesArray={routes} />
+            <div />
+          </div>
+        ) : (
+          safeRoutesArray.map((route, index) => (
+            <Link
+              to={route.routeUrl}
+              key={index}
+              className="navLink"
+              data-testid="nav-links"
+            >
+              {route.name}
+            </Link>
+          ))
+        ))}
+      {safeRoutesArray && safeRoutesArray.length == 0 && (
+        <div className="navbarMinimumHeight" />
       )}
     </div>
   );
