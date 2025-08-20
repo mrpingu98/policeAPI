974dcf4f299181987467780c9076ec5c66165992
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-08-20 14:44:35 +0100

    placeholder for unit test so it doesn't fail

--- src/Pages/API/Api.spec.tsx
+++ src/Pages/API/Api.spec.tsx
@@ -8,3 +8,5 @@
 //tests outside of just isolation
 //click submit button, response comes back, crimse section should render with information from response
 //click on map, and location is updated
+
+import { render } from "@testing-library/react";
