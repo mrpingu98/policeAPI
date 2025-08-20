c2f27f30f3adaefb77f4f948546ddbf6f44dd399
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-08-20 14:26:29 +0100

    planning unit tests

--- src/Pages/API/Api.spec.tsx
+++ src/Pages/API/Api.spec.tsx
@@ -1,1 +1,10 @@
 //placeholder
+
+//crimes by location section should appear
+//map component should appear
+//input for date should appear
+//clickable submit button
+
+//tests outside of just isolation
+//click submit button, response comes back, crimse section should render with information from response
+//click on map, and location is updated
