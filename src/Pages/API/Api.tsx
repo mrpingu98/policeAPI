1524449b52405111355a801ab441a0693998b0eb
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-08-20 15:41:03 +0100

    pulling map onto api page

--- src/Pages/API/Api.tsx
+++ src/Pages/API/Api.tsx
@@ -1,6 +1,14 @@
 import React from "react";
+import Map from "../../Components/Map/Map";
+import "../../Styling/components/map.scss";
+
 const Api: React.FC = () => {
-  return <div>Api page</div>;
+  return (
+    <div>
+      Api page
+      <Map />
+    </div>
+  );
 };
 
 export default Api;
