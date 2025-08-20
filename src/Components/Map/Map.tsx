b159f790c4ad30ffbc3088f47f75f05010b1b3f1
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-08-20 16:06:18 +0100

    adding props to map component

--- src/Components/Map/Map.tsx
+++ src/Components/Map/Map.tsx
@@ -2,18 +2,29 @@ import React, { useEffect } from "react";
 import "../../Styling/components/map.scss";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import { LatLng } from "../Types";
 
-const Map: React.FC = () => {
+interface MapProps {
+  setLatitudeLongitude: React.Dispatch<React.SetStateAction<LatLng>>;
+}
+
+const Map: React.FC<MapProps> = ({ setLatitudeLongitude }) => {
   useEffect(() => {
-    const map = L.map("map").setView([51.505, -0.09], 13);
+    const map = L.map("map").setView([52.815, -1.126], 6);
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
       maxZoom: 19,
       attribution:
         '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     }).addTo(map);
 
+    function onMapClick(e: L.LeafletMouseEvent) {
+      setLatitudeLongitude(e.latlng);
+    }
+
+    map.on("click", onMapClick);
+
     return () => {
-      map.remove(); // cleanup
+      map.remove();
     };
   }, []);
 
