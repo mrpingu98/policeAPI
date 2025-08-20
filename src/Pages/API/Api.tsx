870cb5309f9a8226ce3d3a902147a2083465c7aa
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-08-20 16:06:08 +0100

    adding usestates to hold latlng

--- src/Pages/API/Api.tsx
+++ src/Pages/API/Api.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Map from "../../Components/Map/Map";
 import "../../Styling/components/map.scss";
+import { LatLng } from "../../Components/Types";
 
 const Api: React.FC = () => {
+  const [latitudeLongitude, setLatitudeLongitude] = useState<LatLng>({
+    lat: 0,
+    lng: 0,
+  });
   return (
     <div>
-      Api page
-      <Map />
+      <Map setLatitudeLongitude={setLatitudeLongitude} />
+      <div>
+        lat: {latitudeLongitude.lat} long: {latitudeLongitude.lng}
+      </div>
     </div>
   );
 };
