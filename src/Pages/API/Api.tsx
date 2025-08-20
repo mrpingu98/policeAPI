7826457f1bae0ea0ba75e0e42c2100b419543d37
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-08-20 16:14:47 +0100

    adding placeholder components to the page

--- src/Pages/API/Api.tsx
+++ src/Pages/API/Api.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import Map from "../../Components/Map/Map";
 import "../../Styling/components/map.scss";
 import { LatLng } from "../../Components/Types";
+import Textbox from "../../Components/Textbox/Textbox";
+import { Button } from "../../Components/Button/Button";
 
 const Api: React.FC = () => {
   const [latitudeLongitude, setLatitudeLongitude] = useState<LatLng>({
@@ -14,6 +16,12 @@ const Api: React.FC = () => {
       <div>
         lat: {latitudeLongitude.lat} long: {latitudeLongitude.lng}
       </div>
+      <Textbox />
+      <Button
+        text="Submit"
+        variant="primary"
+        onClick={() => console.log("submitted")}
+      />
     </div>
   );
 };
