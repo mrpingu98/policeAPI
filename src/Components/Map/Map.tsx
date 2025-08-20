63fbdc49e7f2034b309bcd09849763292db426ca
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-08-20 15:40:44 +0100

    basic map view setup

--- src/Components/Map/Map.tsx
+++ src/Components/Map/Map.tsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "../../Styling/components/map.scss";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
 const Map: React.FC = () => {
-  return <div>This will be a map</div>;
+  useEffect(() => {
+    const map = L.map("map").setView([51.505, -0.09], 13);
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map);
+
+    return () => {
+      map.remove(); // cleanup
+    };
+  }, []);
+
+  return <div id="map" className="mapContainer"></div>;
 };
 
 export default Map;
