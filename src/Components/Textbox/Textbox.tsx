24b5fa4399bfcee283c68975c7d974a974add36e
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-08-26 15:47:07 +0100

    removing unecessary attribute

--- src/Components/Textbox/Textbox.tsx
+++ src/Components/Textbox/Textbox.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import "../../Styling/components/textbox.scss"
+import "../../Styling/components/textbox.scss";
 
 const Textbox: React.FC = () => {
-  return <input type="text" name="date" className="textbox"/>;
+  return <input type="text" className="textbox" />;
 };
 
 export default Textbox;
