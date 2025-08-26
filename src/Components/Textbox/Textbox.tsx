def2c0f4621320b30e83fd23b1098d5d69c336b1
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-08-26 16:32:03 +0100

    safe checks to props

--- src/Components/Textbox/Textbox.tsx
+++ src/Components/Textbox/Textbox.tsx
@@ -8,14 +8,18 @@ interface TextboxProps {
 }
 
 const Textbox: React.FC<TextboxProps> = ({ name, setText, type }) => {
-  return (
+  const safeType = type.trim() || false;
+  const safeName = name?.trim() || false;
+
+  return safeType && safeName ? (
     <input
       type={type}
       className="textbox"
       name={name}
       onChange={(e) => setText(e.target.value)}
+      data-testid="textbox"
     />
-  );
+  ) : null;
 };
 
 export default Textbox;
