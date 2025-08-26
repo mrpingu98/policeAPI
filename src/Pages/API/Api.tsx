d3356acf025629a8bfaad0f0262a54c7e4c7957c
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-08-26 15:54:17 +0100

    adding textbox props

--- src/Pages/API/Api.tsx
+++ src/Pages/API/Api.tsx
@@ -10,17 +10,23 @@ const Api: React.FC = () => {
     lat: 0,
     lng: 0,
   });
+
+  const [date, setDate] = useState<string>("");
+  console.log(date);
+
   return (
     <div>
       <h1>Crimes by location</h1>
-      <div>Use the map below to find out about crimes by location across the UK. Pick a point on the map 
-        and input a date to find out the recorded crimes at that location at that time.
+      <div>
+        Use the map below to find out about crimes by location across the UK.
+        Pick a point on the map and input a date to find out the recorded crimes
+        at that location at that time.
       </div>
       <Map setLatitudeLongitude={setLatitudeLongitude} />
       <div>
         lat: {latitudeLongitude.lat} long: {latitudeLongitude.lng}
       </div>
-      <Textbox />
+      <Textbox setText={setDate} />
       <Button
         text="Submit"
         variant="primary"
