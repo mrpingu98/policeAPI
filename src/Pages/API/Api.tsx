e995e9c8f09c658caa43acb18b2aec1743524010
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-08-28 16:32:01 +0100

    desktop view styling

--- src/Pages/API/Api.tsx
+++ src/Pages/API/Api.tsx
@@ -16,34 +16,42 @@ const Api: React.FC = () => {
   return (
     <div>
       <h2>Crimes by location</h2>
-      <p>
+      <p className="description">
         Use the map below to find out about crimes by location across the UK.
         Pick a point on the map and input a date to find out the recorded crimes
         at that location at that time.
       </p>
-      <Map setLatitudeLongitude={setLatitudeLongitude} />
-      <div className="sectionContainers" data-testId="location">
-        <p>
-          <b>Location:</b>{" "}
-          {latitudeLongitude ? (
-            `Latitude: ${latitudeLongitude.lat}, Longitude: ${latitudeLongitude.lng}`
-          ) : (
-            <em>Select a point on the map</em>
-          )}
-        </p>
-      </div>
-      <div className="sectionContainers">
-        <p>
-          <b>Date:</b>
-        </p>
-        <DatePickerMonth setDate={setDate} />
-      </div>
-      <div className="button">
-        <Button
-          text="Submit"
-          variant="primary"
-          onClick={() => console.log("submitted")}
-        />
+      <div className="desktopContainer">
+        <div className="mapContainer" data-testId="location">
+          <Map setLatitudeLongitude={setLatitudeLongitude} />
+        </div>
+        <div className="formContainer">
+          <div className="sectionContainers" data-testId="location">
+            <p>
+              <b>Location:</b>{" "}
+              {latitudeLongitude ? (
+                `Latitude: ${latitudeLongitude.lat.toFixed(
+                  5
+                )}, Longitude: ${latitudeLongitude.lng.toFixed(5)}`
+              ) : (
+                <em>Select a point on the map</em>
+              )}
+            </p>
+          </div>
+          <div className="sectionContainers" data-testId="datePicker">
+            <p>
+              <b>Date:</b>
+            </p>
+            <DatePickerMonth setDate={setDate} />
+          </div>
+          <div className="button">
+            <Button
+              text="Submit"
+              variant="primary"
+              onClick={() => console.log("submitted")}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
