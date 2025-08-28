cfbbacbe38dc285fe5c5668d85d71a2e75db5351
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-08-28 14:44:57 +0100

    drafting unit tests for page

--- src/Pages/API/Api.spec.tsx
+++ src/Pages/API/Api.spec.tsx
@@ -3,10 +3,12 @@
 //crimes by location section should appear
 //map component should appear
 //input for date should appear
+//form should appear
 //clickable submit button
 
-//tests outside of just isolation
-//click submit button, response comes back, crimse section should render with information from response
+//integration tests
 //click on map, and location is updated
+//click map (on a specific point), input date, click submit button, response comes back, recorded crimes section should render with information from response
+//input date too far in past, click submit, error message should appear
 
 import { render } from "@testing-library/react";
