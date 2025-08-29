51e21dad6fab5c465c9dedbbef2e1fa926d53047
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-08-29 19:46:43 +0100

    loading circle component

---
+++ src/Components/LoadingCircle/LoadingCircle.tsx
@@ -0,0 +1,8 @@
+import "../../Styling/components/loadingCircle.scss";
+import React from "react";
+
+const LoadingCircle = () => {
+  return <span className="loader" />;
+};
+
+export { LoadingCircle };
