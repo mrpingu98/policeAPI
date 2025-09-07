5a9051e71b1825d21ba2af6bc8db0042d3f784b3
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-09-07 10:52:00 +0100

    changing api hit

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,1 +1,2 @@
-export const baseUrl = "https://data.police.uk/api/crimes-street/all-crime";
+export const baseUrl = "https://data.police.uk/api/crimes-at-location";
+// "https://data.police.uk/api/crimes-street/all-crime";
