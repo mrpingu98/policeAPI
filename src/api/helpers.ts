5181e64fd0e64437c0db52c36ded2a93569e5745
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-09-30 10:58:46 +0100

    simplifying error handling

--- src/api/helpers.ts
+++ src/api/helpers.ts
@@ -1,57 +1,28 @@
 import { baseUrl } from "../constants/api";
-import { NetworkError, HttpError } from "../Components/Types";
+// import { NetworkError, HttpError } from "../Components/Types";
 
 interface KeyValue {
   key: string;
   value: string;
 }
 
-export function queryParamsHelperFunction(
-  baseUrl: string,
-  queryParameters: KeyValue[]
-) {
-  const joinKeyValuePairs = queryParameters.map(
-    (object) => `${object.key}=${object.value}`
-  );
+export function queryParamsHelperFunction(baseUrl: string, queryParameters: KeyValue[]) {
+  const joinKeyValuePairs = queryParameters.map((object) => `${object.key}=${object.value}`);
   const joinAllQueryParams = joinKeyValuePairs.join("&");
   const requestUrl = `${baseUrl}?${joinAllQueryParams}`;
 
   return requestUrl;
 }
 
-export async function getRequest(
-  urlAffix: string,
-  queryParameters?: KeyValue[]
-) {
+export async function getRequest(urlAffix: string, queryParameters?: KeyValue[]) {
   try {
     const response = await fetch(
-      queryParameters
-        ? queryParamsHelperFunction(`${baseUrl}${urlAffix}`, queryParameters)
-        : `${baseUrl}${urlAffix}`
+      queryParameters ? queryParamsHelperFunction(`${baseUrl}${urlAffix}`, queryParameters) : `${baseUrl}${urlAffix}`
     );
-    //-- HTTP error
-    // if (!response.ok) {
-    //   let errorMessage = "Unknown error details";
-    //   try {
-    //     const data = await response.json();
-    //     if (data?.error) errorMessage = data.error;
-    //   } catch {
-    //     //ignore error
-    //   }
-    //   throw new HttpError(response.status, errorMessage);
-    // }
-    //-- Success
     const data = await response.json();
     return data;
-  } catch (err: unknown) {
-    if (err instanceof HttpError) {
-      throw err;
-    }
-    //-- Network error
-    if (err instanceof Error) {
-      throw new NetworkError(`Network error ${err.message}`);
-    }
-    throw new Error("Network error occurred");
+  } catch (err) {
+    throw err as Error;
   }
 }
 
@@ -68,3 +39,34 @@ export async function getRequest(
 
 //professional project would have an error boundary - throw errors, boundary catches them and shows an error screen
 //catch - type of exception - to handle specific errors catch
+
+// export async function getRequest(urlAffix: string, queryParameters?: KeyValue[]) {
+//   try {
+//     const response = await fetch(
+//       queryParameters ? queryParamsHelperFunction(`${baseUrl}${urlAffix}`, queryParameters) : `${baseUrl}${urlAffix}`
+//     );
+//     -- HTTP error
+//     if (!response.ok) {
+//       let errorMessage = "Unknown error details";
+//       try {
+//         const data = await response.json();
+//         if (data?.error) errorMessage = data.error;
+//       } catch {
+//         //ignore error
+//       }
+//       throw new HttpError(response.status, errorMessage);
+//     }
+//     //-- Success
+//     const data = await response.json();
+//     return data;
+//   } catch (err: unknown) {
+//     if (err instanceof HttpError) {
+//       throw err;
+//     }
+//     //-- Network error
+//     if (err instanceof Error) {
+//       throw new NetworkError(`Network error ${err.message}`);
+//     }
+//     throw new Error("Network error occurred");
+//   }
+// }
