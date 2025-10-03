af018445c89b0b2f50c4f793b7b40aa185271e73
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-10-03 14:22:51 +0100

    updating error unit tests

--- src/Components/Error/Error.tsx
+++ src/Components/Error/Error.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import "../../Styling/components/error.scss";
+import { isStringNotEmpty } from "../../utils/strings/string";
 
 interface ErrorProps {
   errorMessage: string;
 }
 
 const Error: React.FC<ErrorProps> = ({ errorMessage }) => {
-  return <div className="errorContainer">{errorMessage}</div>;
+  const safeErrorMessage = isStringNotEmpty(errorMessage);
+  const genericErrorMessage = "An error occurred";
+
+  return (
+    <div className="errorContainer" data-testid="error-container">
+      {safeErrorMessage ? safeErrorMessage : genericErrorMessage}
+    </div>
+  );
 };
 
 export { Error };
